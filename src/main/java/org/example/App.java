b7c41e5f653621ee7ce6a897b82c6ffa2df35c6f
Annahita <aalipourannahita@gmail.com>
2024-06-23 16:10:51 +0330

    use for each

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,7 +1,6 @@
 package org.example;
 
 import java.util.Arrays;
-import java.util.Locale;
 import java.util.Scanner;
 
 
@@ -11,41 +10,35 @@ public class App
     {
         System.out.print("Print Your Word: ");
         Scanner scan1 = new Scanner(System.in);
-        String word = scan1.next();
+        String word = scan1.next().toLowerCase();
         System.out.print("Print Your Sentence: ");
         Scanner scan2 = new Scanner(System.in);
-        String sentence = scan2.nextLine();
+        String sentence = scan2.nextLine().toLowerCase();
 
-        word = word.toLowerCase();
-        sentence = sentence.toLowerCase();
+
 
         String[] words = sentence.split(" ");
-
-        String result = "No Anagram Found";
         int count = 0;
-        for (int i = 0; i < words.length; i++) {
 
-            if (words[i].length() == word.length()){
 
-                char[] charWord = word.toCharArray();
-                char[] charWords = words[i].toCharArray();
+        for (String candidate : words) {
+            if (candidate.length() == word.length()) {
+                char[] sortedWord = word.toCharArray();
+                char[] sortedCandidate = candidate.toCharArray();
+                Arrays.sort(sortedWord);
+                Arrays.sort(sortedCandidate);
 
-                Arrays.sort(charWord);
-                Arrays.sort(charWords);
-
-                if(Arrays.equals(charWord,charWords)){
-                    System.out.println(String.format("%s and %s are Anagram",word,words[i]));
+                if (Arrays.equals(sortedWord, sortedCandidate)) {
+                    System.out.println(String.format("%s and %s are anagrams", word, candidate));
                     count++;
                 }
-
             }
-
-
-            }
-        if (count !=0){
-            result= String.format("%d Anagram found",count);
         }
-        System.out.println(result);
+
+        if (count !=0){
+            System.out.println(String.format("%d anagram(s) found", count));
+        }
+        System.out.println("No anagrams found.");
 
 
     }
